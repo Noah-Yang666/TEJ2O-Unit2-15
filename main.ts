bde0d6882b360f127ebe1e5b45eb823add061d1c
Noah-Yang666 <noah.yang@stu.ocsb.ca>
2026-04-12 16:28:02 -0400

    program uses nest loops to move sprite around microbit

--- main.ts
+++ main.ts
@@ -1,8 +1,36 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Noah Yang
+ * Created on: April 2026
+ * This program uses nest loops to move sprite around microbit
 */
 
-basic.showString('Hello, World!')
+// variables
+let loopCounter1 = 0
+let loopCounter2 = 0
+let sprite: game.LedSprite = null
+
+// setting up
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// when A is pressed, sprite moves clockwise
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    sprite = game.createSprite(0, 0)
+    loopCounter1 = 0
+    loopCounter2 = 0
+    while (loopCounter1 < 4) {
+        loopCounter1++
+        loopCounter2 = 0
+        while (loopCounter2 < 4) {
+            sprite.move(1)
+            basic.pause(300)
+            loopCounter2++
+        }
+        sprite.turn(Direction.Right, 90)
+    }
+    sprite.delete()
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
